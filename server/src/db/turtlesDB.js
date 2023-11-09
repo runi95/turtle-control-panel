f98fee8df4731ee07258e634b356b4f59e8cfb51
runi95 <runar.serigstad@gmail.com>
2023-11-09 22:51:46 +0100

    Added several more turtleDB update functions

--- server/src/db/turtlesDB.js
+++ server/src/db/turtlesDB.js
@@ -10,14 +10,6 @@ class TurtlesDB {
         await this.db.push(`/${turtle.id}`, turtle);
     }
 
-    async updateOnlineStatus(id, isOnline) {
-        await this.db.push(`/${id}/isOnline`, isOnline);
-    }
-
-    async updateState(id, state) {
-        await this.db.push(`/${id}/state`, state);
-    }
-
     async getTurtle(id) {
         try {
             return await this.db.getData(`/${id}`);
@@ -28,6 +20,42 @@ class TurtlesDB {
 
     async getTurtles() {
         return await this.db.getData('/');
+    }
+
+    async updateName(id, name) {
+        await this.db.push(`/${id}/name`, name);
+    }
+
+    async updateOnlineStatus(id, isOnline) {
+        await this.db.push(`/${id}/isOnline`, isOnline);
+    }
+
+    async updateFuelLevel(fuelLevel) {
+        await this.db.push(`/${id}/fuelLevel`, fuelLevel);
+    }
+
+    async updateSelectedSlot(selectedSlot) {
+        await this.db.push(`/${id}/selectedSlot`, selectedSlot);
+    }
+
+    async updateInventory(inventory) {
+        await this.db.push(`/${id}/inventory`, inventory);
+    }
+
+    async updateStepsSinceLastRecharge(stepsSinceLastRecharge) {
+        await this.db.push(`/${id}/stepsSinceLastRecharge`, stepsSinceLastRecharge);
+    }
+
+    async updateState(id, state) {
+        await this.db.push(`/${id}/state`, state);
+    }
+
+    async updateLocation(location) {
+        await this.db.push(`/${id}/location`, location);
+    }
+
+    async updateDirection(direction) {
+        await this.db.push(`/${id}/direction`, direction);
     }
 }
 
