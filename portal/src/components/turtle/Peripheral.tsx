68d13476c632fb6299108c237e86f2e08c928b29
runi95 <runar.serigstad@gmail.com>
2023-12-30 22:51:56 +0100

    Added peripheral groups to make peripheral origin more obvious

--- portal/src/components/turtle/Peripheral.tsx
+++ portal/src/components/turtle/Peripheral.tsx
@@ -1,3 +1,4 @@
+import styled from 'styled-components';
 import {Button} from 'react-bootstrap';
 import {Action, Turtle} from '../../App';
 
@@ -13,35 +14,56 @@ function Peripheral(props: PeripheralProps) {
     const {id} = turtle;
     if (types.includes('modem')) {
         return (
-            <div>
-                <Button
-                    onClick={() => action({type: 'ACTION', action: 'locate', data: {id}})}
-                    variant={turtle.location === null ? 'outline-success' : 'outline-secondary'}
-                    size='sm'
-                    disabled={!turtle.isOnline}
-                >
-                    Locate
-                </Button>
-            </div>
+            <PeripheralGroup>
+                <div style={{marginBottom: 8}}>
+                    <small>Modem</small>
+                </div>
+                <PeripheralContainer>
+                    <Button
+                        onClick={() => action({type: 'ACTION', action: 'locate', data: {id}})}
+                        variant={turtle.location === null ? 'outline-success' : 'outline-secondary'}
+                        size='sm'
+                        disabled={!turtle.isOnline}
+                    >
+                        Locate
+                    </Button>
+                </PeripheralContainer>
+            </PeripheralGroup>
         );
     }
 
     if (types.includes('geoScanner')) {
         return (
-            <div>
-                <Button
-                    onClick={() => action({type: 'ACTION', action: 'scan', data: {id}})}
-                    variant='outline-info'
-                    size='sm'
-                    disabled={!turtle.isOnline}
-                >
-                    Scan
-                </Button>
-            </div>
+            <PeripheralGroup>
+                <div style={{marginBottom: 8}}>
+                    <small>Geo Scanner</small>
+                </div>
+                <PeripheralContainer>
+                    <Button
+                        onClick={() => action({type: 'ACTION', action: 'scan', data: {id}})}
+                        variant='outline-info'
+                        size='sm'
+                        disabled={!turtle.isOnline}
+                    >
+                        Scan
+                    </Button>
+                </PeripheralContainer>
+            </PeripheralGroup>
         );
     }
 
     return null;
 }
+
+const PeripheralGroup = styled.div`
+    border: 1px solid rgba(255, 255, 255, 0.25);
+    padding: 8px;
+    border-radius: 6%;
+`;
+
+const PeripheralContainer = styled.div`
+    display: flex;
+    gap: 5px;
+`;
 
 export default Peripheral;
