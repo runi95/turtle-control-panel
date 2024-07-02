dbe35a0d9b3dbdf7b81ae6e25146088f6a17410c
runi95 <runar.serigstad@gmail.com>
2024-07-03 01:32:13 +0200

    Vastly improved Grabcraft block recognition

--- portal/src/components/turtle/grabcraft/GrabcraftModal.tsx
+++ portal/src/components/turtle/grabcraft/GrabcraftModal.tsx
@@ -2,97 +2,27 @@ import {useState} from 'react';
 import {Modal, Form, Button, Row, Col} from 'react-bootstrap';
 import './GrabcraftModal.css';
 import {httpServerUrl} from '../../../api';
-import {useAtlasMap} from '../Turtle3DMap/TextureAtlas';
 import {Block} from '../../../App';
+import {grabcraftNameToBlockMap} from './GrabcraftBlockNames';
 
-const jaroWinklerDistance = (a: string, b: string): number => {
-    const matches = (() => {
-        const bound = Math.floor(Math.max(a.length, b.length) / 2) - 1;
-        const matched = [];
-        let matches = 0;
-        for (let i = 0; i < a.length; i++) {
-            for (let j = Math.max(0, i - bound); j <= Math.min(b.length, i + bound); j++) {
-                if (a[i] === b[j] && !matched[j]) {
-                    matched[j] = true;
-                    matches++;
-                    break;
-                }
-            }
+const levenshteinDistance = (a: string, b: string): number => {
+    const arr = [];
+    for (let i = 0; i <= b.length; i++) {
+        arr[i] = [i];
+        for (let j = 1; j <= a.length; j++) {
+            arr[i][j] =
+                i === 0
+                    ? j
+                    : Math.min(
+                          arr[i - 1][j] + 1,
+                          arr[i][j - 1] + 1,
+                          arr[i - 1][j - 1] + (a[j - 1] === b[i - 1] ? 0 : 1)
+                      );
         }
-
-        return matches;
-    })();
-
-    if (matches > 0) {
-        const transpositions = (() => {
-            const bound = Math.max(a.length, b.length);
-
-            let aMatches = '';
-            const aMatched = [];
-            for (let i = 0; i < a.length; i++) {
-                for (let j = Math.max(0, i - bound); j <= Math.min(b.length, i + bound); j++) {
-                    if (a[i] === b[j] && !aMatched[j]) {
-                        aMatches += a[i];
-                        aMatched[j] = true;
-                        break;
-                    }
-                }
-            }
-
-            let bMatches = '';
-            const bMatched = [];
-            for (let i = 0; i < b.length; i++) {
-                for (let j = Math.max(0, i - bound); j <= Math.min(a.length, i + bound); j++) {
-                    if (b[i] === a[j] && !bMatched[j]) {
-                        bMatches += b[i];
-                        bMatched[j] = true;
-                        break;
-                    }
-                }
-            }
-
-            let transpositions = 0;
-            for (let i = 0; i < aMatches.length; i++) {
-                if (aMatches[i] !== bMatches[i]) transpositions++;
-            }
-
-            return Math.floor(transpositions / 2);
-        })();
-
-        const similarity = (matches / a.length + matches / b.length + (matches - transpositions) / matches) / 3;
-        if (similarity < 0.7) return similarity;
-
-        const prefix = (() => {
-            for (let i = 0; i < a.length && i < b.length; i++) {
-                if (a[i] !== b[i]) return i;
-            }
-
-            return Math.min(a.length, b.length);
-        })();
-        return similarity + prefix * 0.1 * (1 - similarity);
     }
 
-    return 0;
+    return arr[b.length][a.length];
 };
-
-// const levenshteinDistance = (a: string, b: string): number => {
-//     const arr = [];
-//     for (let i = 0; i <= b.length; i++) {
-//         arr[i] = [i];
-//         for (let j = 1; j <= a.length; j++) {
-//             arr[i][j] =
-//                 i === 0
-//                     ? j
-//                     : Math.min(
-//                           arr[i - 1][j] + 1,
-//                           arr[i][j - 1] + 1,
-//                           arr[i - 1][j - 1] + (a[j - 1] === b[i - 1] ? 0 : 1)
-//                       );
-//         }
-//     }
-
-//     return arr[b.length][a.length];
-// };
 
 type Props = {
     hideModal: () => void;
@@ -119,7 +49,6 @@ function GrabcraftModal({hideModal, onBuild}: Props) {
     const [isLoading, setIsLoading] = useState(false);
     const [grabcraftUrl, setGrabcraftUrl] = useState<string>('');
     const [buildData, setBuildData] = useState<BuildData | null>(null);
-    const {data: atlasMap} = useAtlasMap();
 
     const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
@@ -167,47 +96,38 @@ function GrabcraftModal({hideModal, onBuild}: Props) {
 
                     let largestX = 0;
                     let largestZ = 0;
+                    const mapKeys = Object.keys(grabcraftNameToBlockMap);
                     for (const y in renderObject) {
                         for (const x in renderObject[y]) {
                             for (const z in renderObject[y][x]) {
-                                const {name: grabcraftName} = renderObject[y][x][z];
-                                const grabcraftNameLowered = grabcraftName
-                                    .toLowerCase()
+                                const grabcraftName = renderObject[y][x][z].name
                                     .replaceAll(new RegExp('\\([^)]+\\)', 'g'), '')
-                                    .replaceAll(' ', '_');
+                                    .trim();
 
                                 // Always skip layer 1 Dirt as it's not part of the actual build
                                 if (y === '1' && grabcraftName === 'Dirt') continue;
                                 if (y === '1' && grabcraftName === 'Still Water') continue;
                                 if (grabcraftName === 'Grass') continue;
-                                if (grabcraftName === 'Water') continue;
 
                                 const name = (() => {
-                                    const blockName = grabcraftNameToBlockNameMap.get(grabcraftNameLowered);
+                                    const blockName = grabcraftNameToBlockNameMap.get(grabcraftName);
                                     if (blockName != null) {
                                         return blockName;
                                     }
 
-                                    if (atlasMap == null) return null;
-                                    let currentBestMatch = 0;
+                                    let currentBestMatch = Number.MAX_VALUE;
                                     let bestStringMatch: string | null = null;
-                                    for (const texture in atlasMap.textures) {
-                                        if (texture === 'unknown') continue;
-
-                                        const split = texture.split(':');
-                                        if (split.length !== 2) continue;
-                                        if (split[0] !== 'minecraft') continue;
-
-                                        const distance = jaroWinklerDistance(grabcraftNameLowered, split[1]);
-                                        if (distance > currentBestMatch) {
+                                    for (const key of mapKeys) {
+                                        const distance = levenshteinDistance(grabcraftName, key);
+                                        if (distance < currentBestMatch) {
                                             currentBestMatch = distance;
-                                            bestStringMatch = texture;
+                                            bestStringMatch = grabcraftNameToBlockMap[key];
                                         }
                                     }
 
                                     if (bestStringMatch == null) return null;
 
-                                    grabcraftNameToBlockNameMap.set(grabcraftNameLowered, bestStringMatch);
+                                    grabcraftNameToBlockNameMap.set(grabcraftName, bestStringMatch);
                                     return bestStringMatch;
                                 })();
 
