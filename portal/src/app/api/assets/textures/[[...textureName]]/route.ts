f74af830f74fd269a50efaf85643dc73fcd9ec26
runi95 <runar.serigstad@gmail.com>
2026-02-15 12:35:35 +0100

    Fixed issues with initial call to textures route

--- portal/src/app/api/assets/textures/[[...textureName]]/route.ts
+++ portal/src/app/api/assets/textures/[[...textureName]]/route.ts
@@ -5,11 +5,18 @@ import unknown from "./unknown";
 
 const pathSubstrLength = "/api/assets/textures/".length;
 
-const readTextureCache = new Promise<Textures>((resolve, reject) => {
-  fs.readFile("assets/textures.json", "utf-8")
-    .then((textures) => resolve(JSON.parse(textures) as Textures))
-    .catch((err) => reject(err));
-});
+let cachedTextures: Textures | null = null;
+
+const readTextureCache = async () => {
+  if (cachedTextures != null) return cachedTextures;
+
+  const textures = await fs
+    .readFile("assets/textures.json", "utf-8")
+    .then((textures) => JSON.parse(textures) as Textures);
+  cachedTextures = textures;
+
+  return textures;
+};
 
 export async function GET(request: NextRequest) {
   const url = new URL(request.url);
@@ -23,7 +30,7 @@ export async function GET(request: NextRequest) {
     });
   }
 
-  const textures = await readTextureCache;
+  const textures = await readTextureCache();
   const texture = textures[textureName];
   if (texture == null) {
     return new Response("Not Found", {
